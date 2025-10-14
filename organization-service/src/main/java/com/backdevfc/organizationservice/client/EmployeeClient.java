c38bcd2997acc73129712a430ca3a77820dc0d37
gibbssonfarias30 <gibbfarc17@gmail.com>
2025-10-14 17:04:20 -0500

    feat: add Feign clients for inter-service communication

---
+++ organization-service/src/main/java/com/backdevfc/organizationservice/client/EmployeeClient.java
@@ -0,0 +1,17 @@
+package com.backdevfc.organizationservice.client;
+
+import com.backdevfc.organizationservice.model.Employee;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+
+@FeignClient(name = "employee-service")
+public interface EmployeeClient {
+
+	@GetMapping("/organization/{organizationId}")
+	List<Employee> findByOrganization(@PathVariable("organizationId") Long organizationId);
+	
+}
